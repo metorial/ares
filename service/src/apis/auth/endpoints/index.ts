4554aeee51e6a26e0bf0f6b9772b735601d5d143
Tobias Herber <22559657+herber@users.noreply.github.com>
2026-02-14 18:04:24 +0100

    fix imports

--- service/src/apis/auth/endpoints/index.ts
+++ service/src/apis/auth/endpoints/index.ts
@@ -1,9 +1,7 @@
 import { createHono } from '@lowerdeck/hono';
-import { securityHeaders } from '../../../lib/securityHeaders';
 import { authHooksApp } from './hooks';
 import { publicApp } from './public';
 
 export let endpointApp = createHono()
-  .use(securityHeaders)
   .route('/metorial-ares/hooks', authHooksApp)
   .route('', publicApp);
